0a2856508439efbe2e1f31f89ef60827767719e2
juan <jdbetancourth22@misena.edu.co>
2022-03-06 18:45:25 -0500

    proyecto de prueba para realizar los ejercicios con php

---
+++ ajax1.js
@@ -0,0 +1,14 @@
+var btnAjax = document.getElementById('btnAjax');
+btnAjax.addEventListener('click', getDates, false);
+function getDates() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', './texto.txt', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
+            var response = document.getElementById('response');
+            response.innerHTML = `<h1>${this.responseText}</h1>`;
+            }
+        }
+    }
